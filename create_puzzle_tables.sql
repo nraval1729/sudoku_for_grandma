41315d1a803f4636e508516ac1805e833283d019
Nisarg Rawal <nisarg_rawal@brown.edu>
2016-06-03 00:58:02 -0400

    Created database of puzzles. Categorized by difficulty levels

---
+++ create_puzzle_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE warmup_puzzles
+(
+	puzzle_string TEXT
+);
+
+CREATE TABLE easy_puzzles
+(
+	puzzle_string TEXT
+);
+
+CREATE TABLE medium_puzzles
+(
+	puzzle_string TEXT
+);
+
+CREATE TABLE hard_puzzles
+(
+	puzzle_string TEXT
+);
+
+CREATE TABLE insane_puzzles
+(
+	puzzle_string TEXT
+);
+
